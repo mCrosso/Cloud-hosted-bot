f13aac9cce1f0fb59a37af524f320eb459248a4d
mCrosso <macrosso2008@gmail.com>
2020-06-26 10:00:56 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from discord.ext import commands
 client = commands.Bot(command_prefix='.')
 client.remove_command('help')
 
+
 @client.event
 async def on_ready():
     print('Bot is running.')
@@ -132,12 +133,64 @@ async def snipe(ctx):
     await ctx.send(embed=embed)
 
 
+@client.command(aliases=['8ball2'])
+async def _8ball2(ctx):
+    randval = random.randint(0, 100)
+    emotesnum = int(str(round(randval / 10)))
+    black = '<:NewProject1:725706978193702933>'
+    purple = '<:purple:725706951425785948>'
+    rest = 10 - emotesnum
+    global possibs
+    possibs = ''
+    if randval <= 10:
+        possibs = "Nope, never"
+    elif 10 <= randval < 20:
+        possibs = "I doubt that"
+    elif 20 <= randval < 30:
+        possibs = "Small chance"
+    elif 30 <= randval < 50:
+        possibs = 'Below average'
+    elif randval == 50:
+        possibs = 'Meh'
+    elif 50 <= randval < 60:
+        possibs = 'Maybe'
+    elif 60 <= randval < 70:
+        possibs = 'Yes by a small chance'
+    elif 70 <= randval < 80:
+        possibs = "Of course"
+    elif 80 <= randval < 90:
+        possibs = "Obviously"
+    elif 90 <= randval <= 100:
+        possibs = 'HELL YEAH'
+    embed = discord.Embed(
+        description=f'{randval}% True ** {emotesnum * purple}{black * rest} {possibs} ** ',
+        color=0x000000)
+    embed.set_author(name=f'Truth Machine', icon_url=ctx.author.avatar_url)
+    await ctx.send(embed=embed)
+
 
 @client.command()
-async def ship(ctx, user: discord.Member, user2: discord.Member = None):
-    if user2 == None:
-        user2 = user
-        user = ctx.author
+async def help(ctx):
+    embed = discord.Embed(
+        color=0x000000, )
+    embed.add_field(name='8ball2', value='A better version of the 8ball command.')
+    embed.add_field(name='ping', value='Return ping.')
+    embed.add_field(name='8ball', value='The original 8ball command.')
+    embed.add_field(name='say', value='Makes the bot say something.')
+    embed.add_field(name='purge', value='Clears some messages.')
+    embed.add_field(name='av', value='Showns user avatar.')
+    embed.add_field(name='pp', value='Shows ur pp size')
+    embed.add_field(name='id', value="Returns user's id")
+    embed.add_field(name='snipe', value='Sends the most recent deleted message.')
+    await ctx.send(embed=embed)
+
+
+@client.command()
+async def ship(ctx, user1, *, user2=None):
+    if user2 is None:
+        user2 = user1
+        user1 = ctx.author.name
+
     randval = random.randint(0, 100)
     emotesnum = int(str(round(randval / 10)))
     rest = 10 - emotesnum
@@ -168,58 +221,9 @@ async def ship(ctx, user: discord.Member, user2: discord.Member = None):
     embed = discord.Embed(
         title=":heartbeat: **MATCHMAKING** :heartbeat:",
         description=(
-            f':arrow_down_small: {user.name} \n :arrow_up_small:  {user2.name} \n \n **{randval}%** {emotesnum * purple}{black * rest} ** {possibs}**'),
+            f':arrow_down_small: {user1} \n :arrow_up_small:  {user2} \n \n **{randval}%** {emotesnum * purple}{black * rest} {possibs}'),
         color=0xff00a2)
     await ctx.send(embed=embed)
 
 
-@client.command(aliases=['8ball2'])
-async def _8ball2(ctx):
-    randval = random.randint(0, 100)
-    emotesnum = int(str(round(randval / 10)))
-    black = '<:NewProject1:725706978193702933>'
-    purple = '<:purple:725706951425785948>'
-    rest = 10 - emotesnum
-    global possibs
-    possibs = ''
-    if randval <= 10:
-        possibs = "Nope, never"
-    elif 10 <= randval < 20:
-        possibs = "I doubt that"
-    elif 20 <= randval < 30:
-        possibs = "Small chance"
-    elif 30 <= randval < 50:
-        possibs = 'Below average'
-    elif randval == 50:
-        possibs = 'Meh'
-    elif 50 <= randval < 60:
-        possibs = 'Maybe'
-    elif 60 <= randval < 70:
-        possibs = 'Yes it by a small chance'
-    elif 70 <= randval < 80:
-        possibs = "Of course"
-    elif 80 <= randval < 90:
-        possibs = "Obviously"
-    elif 90 <= randval <= 100:
-        possibs = 'HELL YEAH'
-    embed = discord.Embed(
-        description=f'{randval}% True ** {emotesnum * purple}{black * rest} {possibs} ** ',
-        color=0x000000)
-    embed.set_author(name=f'Truth Machine', icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=embed)
-
-@client.command()
-async def help(ctx):
-    embed = discord.Embed(
-        color=0x000000,)
-    embed.add_field(name='8ball2', value='A better version of the 8ball command.')
-    embed.add_field(name='ping', value='Return ping.')
-    embed.add_field(name='8ball', value='The original 8ball command.')
-    embed.add_field(name='say', value='Makes the bot say something.')
-    embed.add_field(name='purge', value='Clears some messages.')
-    embed.add_field(name='av', value='Showns user avatar.')
-    embed.add_field(name='pp', value='Shows ur pp size')
-    embed.add_field(name='id', value="Returns user's id")
-    embed.add_field(name='snipe', value='Sends the most recent deleted message.')
-    await ctx.send(embed=embed)
 client.run(os.environ['Discord_Token'])
